4f18ef536b1317542e00536ecd7c4de27b6e74cf
Vlad <lord.qvp@gmail.com>
2017-03-06 18:43:20 +0300

    Add file

---
+++ File_Encoder/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace File_Encoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+            return encryptedBytes;
+        }
+        public void EncryptFile(string file, string pwd)
+        {
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(pwd);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+            File.WriteAllBytes(file, bytesEncrypted);
+        }
+
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+            return decryptedBytes;
+        }
+        public void DecryptFile(string efile, string password)
+        {
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(efile);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+            File.WriteAllBytes(efile, bytesDecrypted);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!(textBox1.Text.Length < 1))
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    DecryptFile(ofd.FileName, textBox1.Text);
+                }
+            } else
+            {
+                MessageBox.Show("Поле пароля не может быть пустым.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!(textBox1.Text.Length < 1))
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    EncryptFile(ofd.FileName, textBox1.Text);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поле пароля не может быть пустым.");
+            }
+        }
+    }
+}
